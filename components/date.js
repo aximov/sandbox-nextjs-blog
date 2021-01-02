c1a58a265e46ed23660c952dd1284850ff36a214
aximov <d@alumni.tus.ac.jp>
2021-01-02 11:48:15 +0900

    ts

--- components/date.js
+++
@@ -1,6 +0,0 @@
-import { parseISO, format } from 'date-fns'
-
-export default function Date({ dateString }) {
-  const date = parseISO(dateString)
-  return <time dateTmie={dateString}>{format(date, 'LLLL d, yyyy')}</time>
-}
